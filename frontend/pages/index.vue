b502850f7cbab27586cd5c9cedf55b9781d77e51
Maximiliano Gutierrez <maxigutierrez23@gmail.com>
2021-11-01 16:23:18 -0300

    First commit

---
+++ frontend/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <Tutorial :message="message" />
+</template>
+
+<script lang="ts">
+export default {
+  async asyncData({ $axios }) {
+    try {
+      const { data } = await $axios.get('/test');
+      return {
+        message: data
+      }
+    } catch (ex) {
+      return {
+        message: ex.response.data.message
+      }
+    }
+  }
+}
+</script>
