b95b0d466a65fabee4b7489c85558e7c8c9b4990
Maximiliano Gutierrez <maxigutierrez23@gmail.com>
2021-11-06 00:11:42 -0300

    Login to dockerhub on builds

--- terraform/codebuild.tf
+++ terraform/codebuild.tf
@@ -104,5 +104,17 @@ resource "aws_codebuild_project" "codebuild" {
       name  = "IMAGE_REPO_NAME"
       value = aws_ecr_repository.tf_ecr.name
     }
+
+    // Use secrets manager on real builds:
+    // https://stackoverflow.com/questions/64967922/docker-hub-login-for-aws-codebuild-docker-hub-limit
+    environment_variable {
+      name  = "DOCKERHUB_USER"
+      value = var.dockerhub_user
+    }
+
+    environment_variable {
+      name  = "DOCKERHUB_PASSWORD"
+      value = var.dockerhub_password
+    }
   }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,9 +4,20 @@ variable "aws_region" {
 }
 
 variable "aws_access_key" {
-  type = string
+  type      = string
+  sensitive = true
 }
 
 variable "aws_secret_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "dockerhub_user" {
   type = string
+}
+
+variable "dockerhub_password" {
+  type      = string
+  sensitive = true
 }
