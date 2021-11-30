a233081b145ad9e42821a6b34816789d442e5e7f
Maximiliano Gutierrez <maxigutierrez23@gmail.com>
2021-11-30 20:32:36 -0300

    Add db debug info on endpoint

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -22,8 +22,15 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/test', function () {
     try {
         DB::connection()->getPdo();
+
         return 'DB connected';
     } catch (\Exception $e) {
-        return 'DB disconnected';
+        $db_connection = env('DB_CONNECTION');
+        $db_database = env('DB_DATABASE');
+        $db_host = env('DB_HOST');
+        $db_port = env('DB_PORT');
+        $db_username = env('DB_USERNAME');
+        $db_password = env('DB_PASSWORD');
+        return "DB disconnected: {$e->getMessage()}, DB_CONNECTION={$db_connection}&DB_DATABASE={$db_database}&DB_HOST={$db_host}&DB_PORT={$db_port}&DB_USERNAME=${db_username}&DB_PASSWORD={$db_password}";
     }
 });
